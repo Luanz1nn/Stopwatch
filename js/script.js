ab96f8d0f6c7ce1cc1760d90d9ffde5dfe72088a
Luanz1nn <luanz1nnwill@gmail.com>
2023-04-05 20:23:51 -0300

    I did the code indentation

--- js/script.js
+++ js/script.js
@@ -1,44 +1,44 @@
-let sec = 0
-let min = 0
-let hr = 0
-let inteval
+let sec = 0;
+let min = 0;
+let hr = 0;
+let inteval;
 
 function twodigits(digíto) {
   if (digíto < 10) {
-    return "0" + digíto
+    return "0" + digíto;
   } else {
-    return digíto
+    return digíto;
   }
 }
 
 function start() {
-  watch()
-  interval = setInterval(watch, 1000)
+  watch();
+  interval = setInterval(watch, 1000);
 }
 
 function pause() {
-  clearInterval(interval)
+  clearInterval(interval);
 }
 
 function stop() {
-  alert(twodigits(hr) + ":" + twodigits(min) + ":" + twodigits(sec))
-  clearInterval(interval)
-  sec = 00
-  min = 00
-  hr = 00
-  document.getElementById("watch").innerText = "00:00:00"
+  alert(twodigits(hr) + ":" + twodigits(min) + ":" + twodigits(sec));
+  clearInterval(interval);
+  sec = 00;
+  min = 00;
+  hr = 00;
+  document.getElementById("watch").innerText = "00:00:00";
 }
 
 function watch() {
-  sec++
+  sec++;
   if (sec == 60) {
-    min++
-    sec = 0
+    min++;
+    sec = 0;
     if (min == 60) {
-      min = 0
-      hr++
+      min = 0;
+      hr++;
     }
   }
   document.getElementById("watch").innerText =
-    twodigits(hr) + ":" + twodigits(min) + ":" + twodigits(sec)
+    twodigits(hr) + ":" + twodigits(min) + ":" + twodigits(sec);
 }
